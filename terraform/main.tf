5b7f65944d71acf448d5fca16431cd9f220372b2
Marty Henderson <marty@ravegrunt.is>
2022-01-06 17:29:43 +0000

    Now with EKS

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,18 +71,14 @@ resource "aws_security_group" "pg-sg"{
   ]
 }
 
-## EC2 Instance
-resource "aws_instance" "webwebserver1" {
-  ami                    = "ami-0d5eff06f840b45e9"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.webserver-sg.id]
-  subnet_id              = aws_subnet.public_subnets
-  tags = {
-    Name = "mjh-demo"
-  }
-}
+
 
 ## RDS Instance
+
+resource "random_password" "db_pass" {
+  length           = 16
+  special          = true
+}
 
 resource "aws_db_instance" "pg-db" {
   allocated_storage         = 5
@@ -90,8 +86,205 @@ resource "aws_db_instance" "pg-db" {
   engine_version            = "13.4"
   instance_class            = "db.t2.micro"
   name                      = "pg-db"
-  username                  = "${var.database_user}"
-  password                  = "${var.database_password}"
+  username                  = var.database_user
+  password                  = random_password.db_pass.result
   db_subnet_group_name      = aws_vpc.main.database_subnets
-  vpc_security_group_ids    = ["${aws_security_group.rds.id}"]
+  vpc_security_group_ids    = aws_security_group.pg-sg
+}
+
+## EKS
+
+resource "aws_iam_role" "eks-cluster" {
+  name = "eks-cluster-role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "e2e-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-cluster.name
+}
+
+resource "aws_iam_role_policy_attachment" "e2e-cluster-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks-cluster.name
+}
+
+resource "aws_security_group" "e2e-cluster" {
+  name        = "terraform-eks-demo-cluster"
+  description = "Cluster communication with worker nodes"
+  vpc_id      = aws_vpc.main.id
+    egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "mjh-demo"
+  }
+}
+
+resource "aws_iam_role" "eks-node" {
+  name = "eks-node"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks-node-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks-node.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-node-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks-node.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-node-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks-node.name
+}
+
+resource "aws_iam_instance_profile" "eks-node" {
+  name = "e2e"
+  role = aws_iam_role.eks-node.name
+}
+
+resource "aws_iam_role" "eks-demo-cluster" {
+  name = "eks-demo-cluster"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks-demo-cluster-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks-demo-cluster.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-demo-cluster-AmazonEKSServicePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  role       = aws_iam_role.eks-demo-cluster.name
+}
+
+resource "aws_eks_cluster" "eks-demo" {
+  name            = var.cluster-name
+  role_arn        = aws_iam_role.eks-demo-cluster.arn
+
+  vpc_config {
+    security_group_ids = aws_security_group.eks-cluster.id
+    subnet_ids         = aws_subnet.private_subnets.*.id
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks-cluster-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.eks-cluster-AmazonEKSServicePolicy,
+  ]
+}
+resource "aws_security_group" "eks-node" {
+  name        = "eks-demo-node"
+  description = "Security group for all nodes in the cluster"
+  vpc_id      = aws_vpc.main.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    Project = "mjh-demo"
+  }
+}
+
+resource "aws_security_group_rule" "eks-ingress-self" {
+  description              = "Allow node to communicate with each other"
+  from_port                = 0
+  protocol                 = "-1"
+  security_group_id        = aws_security_group.eks-node.id
+  source_security_group_id = aws_security_group.eks-node.id
+  to_port                  = 65535
+  type                     = "ingress"
+}
+
+resource "aws_security_group_rule" "eks-ingress-cluster" {
+  description              = "Allow worker Kubelets and pods to receive communication from the cluster control plane"
+  from_port                = 1025
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks-node.id
+  source_security_group_id = aws_security_group.eks-cluster.id
+  to_port                  = 65535
+  type                     = "ingress"
+}
+resource "aws_security_group_rule" "eks-cluster-ingress-node-https" {
+  description              = "Allow pods to communicate with the cluster API Server"
+  from_port                = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.eks-cluster.id
+  source_security_group_id = aws_security_group.eks-node.id
+  to_port                  = 443
+  type                     = "ingress"
+}
+
+resource "aws_launch_configuration" "eks-demo" {
+  associate_public_ip_address = true
+  instance_type               = "m4.large"
+  name_prefix                 = "eks-demo"
+  security_groups             = aws_security_group.eks-node.id
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "aws_autoscaling_group" "eks-demo" {
+  desired_capacity     = 2
+  launch_configuration = aws_launch_configuration.demo.id
+  max_size             = 2
+  min_size             = 1
+  name                 = "terraform-eks-demo"
+  vpc_zone_identifier  = aws_subnet.private_subnets.*.id
+  
+  tags = {
+    Project = "mjh-demo"
+  }
 }
